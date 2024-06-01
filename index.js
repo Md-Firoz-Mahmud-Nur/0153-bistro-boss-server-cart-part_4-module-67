12863ae5fbafd658f82e011fadaa4732ccf0690b
firoz.nur <firoznur5@gmail.com>
2024-06-01 19:08:33 +0600

    Module done

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET_KEY}@cluster0.fp5eepf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -90,5 +91,5 @@ app.listen(port, () => {
  * app.put('/users/:id')
  * app.patch('/users/:id')
  * app.delete('/users/:id')
- * 
+ *
 */
